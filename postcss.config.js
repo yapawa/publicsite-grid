f4de8837f9db9ffcfa8ec529118ff1f63d7f9465
Daniel Muller <github@daniel.ctrlaltdel.ch>
2020-03-08 00:09:49 +0800

    Simple content display

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-    plugins: [
-        require('autoprefixer')({
-            browsers: ['ie >= 8', 'last 3 versions']
-        })
-    ]
-};
+  plugins: [
+    require('autoprefixer')
+  ]
+}

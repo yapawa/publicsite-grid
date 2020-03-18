e86ed8854acbaabe1a543a84764372ff8f7c99af
Daniel Muller <github@daniel.ctrlaltdel.ch>
2020-03-18 22:15:46 +0800

    Tweak deploy script

--- deploy.sh
+++ deploy.sh
@@ -10,10 +10,12 @@ if [ -n ${AWS_REGION+x} ]; then
 fi
 
 aws s3 sync --delete --exclude "_headers" --exclude "_redirects" --cache-control "max-age=31536000" --metadata file://public/_headers --acl public-read public/ s3://$S3_BUCKET/
+aws cloudfront create-invalidation --distribution-id $DISTRIBUTION --paths '/*'
+rm -f /tmp/emptyredirect
 touch /tmp/emptyredirect
+
 cat public/_redirects | grep -v "#.*" | while read line; do
   from=$(echo $line | awk '{print $1}')
   to=$(echo $line | awk '{print $2}')
-  aws s3 cp --cache-control "max-age=31536000" --website-redirect $to --acl public-read /tmp/emptyredirect s3://${S3_BUCKET}${from}index.html
+  aws s3 cp --cache-control \"max-age=31536000\" --website-redirect $to --acl public-read /tmp/emptyredirect s3://${S3_BUCKET}${from}index.html
 done
-aws cloudfront create-invalidation --distribution-id $DISTRIBUTION --paths '/*'

c75a4cf2005c95be56de0386feff4768a89574b0
Daniel Muller <github@daniel.ctrlaltdel.ch>
2020-03-26 17:10:12 +0800

    Tune S3 settings

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,16 @@ if [ -f .env ]; then
   if [ -n "$AWS_PROFILE" ]; then
     export AWS_PROFILE=$AWS_PROFILE
   fi
-  if [ -n "$AWS_REGION" ]; then
-    export AWS_REGION=$AWS_REGION
-  fi
 fi
+
+if [ -n "$AWS_REGION" ]; then
+  export AWS_REGION=$AWS_REGION
+fi
+
+aws configure set default.s3.max_concurrent_requests 40
+aws configure set default.s3.max_queue_size 10000
+aws configure set default.s3.multipart_threshold 64MB
+aws configure set default.s3.multipart_chunksize 16MB
 
 aws s3 sync --delete --exclude "_headers" --exclude "_redirects" --cache-control "max-age=31536000" --metadata file://public/_headers --acl public-read public/ s3://$S3_BUCKET/
 aws cloudfront create-invalidation --distribution-id $DISTRIBUTION --paths '/*'
